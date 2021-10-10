3cba0a733765db13ded7ffc3e2805548f1a84018
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2021-10-10 14:15:15 -0500

    properties example in js file
    types of properties in the js file

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,3 +1,14 @@
 import { LightningElement } from 'lwc';
 
-export default class HelloWorld extends LightningElement {}
+export default class HelloWorld extends LightningElement {
+    name  //undefined
+    age=30  //these local properties are only available in this class
+    fullname="Salesforce Wisconsin"
+    showData =false
+    details={
+        name:"dummy",
+        place:"Melbourne"
+    }
+    userList = ["A","B","C"]
+
+}
