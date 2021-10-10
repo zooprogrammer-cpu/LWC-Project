9a1bac6da3b85cc4abe046c1ade77a4bf772e03a
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2021-10-10 18:41:57 -0500

    Two way data binding
    Input field in HTML gets transferred to JS and back to HTML

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -2,7 +2,14 @@ import { LightningElement } from 'lwc';
 
 export default class HelloWorld extends LightningElement {
     fullname="Zero to Hero"
-    obj={
-        name:"ash"
+    title="aura"
+
+    changeHandler(event){   //event comes from HTML to JS
+        this.title = event.target.value //the word that the user typed goes into event.target.value. 
+        //And title gets updated that sends the value back to the HTML. 
+        //whenever you are accessing 
+        //the property inside a class, you need to use this.propertyname         
     }
+
+    
 }
