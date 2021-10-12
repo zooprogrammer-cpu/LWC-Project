f162a1c1a33d22ee792b55e12fd75d8fe9f675b7
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2021-10-12 07:48:23 -0500

    two way data binding with notes
    two way data binding with notes

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -4,11 +4,12 @@ export default class HelloWorld extends LightningElement {
     fullname="Zero to Hero"
     title="aura"
 
-    changeHandler(event){   //event comes from HTML to JS
+    changeHandler(event){   //changeHandler is a method. changehandler recieves the data we have typed. 
+        //event comes from HTML to JS
         this.title = event.target.value //the word that the user typed goes into event.target.value. 
         //And title gets updated that sends the value back to the HTML. 
         //whenever you are accessing 
-        //the property inside a class, you need to use this.propertyname         
+        //the property inside a class, you need to use this keyword.propertyname         
     }
 
     
