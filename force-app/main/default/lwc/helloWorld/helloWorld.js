c79f40f8821a47c3b138e8f646e6626cb240b734
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2021-10-12 08:12:11 -0500

    @track decorator to update complex data
    @track decorator to update complex data such as objects

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,4 +1,4 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,track } from 'lwc'; //need to add a track here for complex objects tracking
 
 export default class HelloWorld extends LightningElement {
     fullname="Zero to Hero"
@@ -11,6 +11,26 @@ export default class HelloWorld extends LightningElement {
         //whenever you are accessing 
         //the property inside a class, you need to use this keyword.propertyname         
     }
-
     
+
+    //LWC does not automatically update information from complex data types such as Objects- 
+    //Need to add @track which is a decorator. Decorator is a function that wraps anohter function or property. 
+    @track address={
+        city: 'Madison',
+        postcode: 53718,
+        country:'USA'
+    }
+
+    trackHandler(event){
+        this.address.city =event.target.value
+
+
+    }
+
+    //good principle to not overwrite the object as above. Make a new copy of the object
+ 
+    
+
+
+
 }
