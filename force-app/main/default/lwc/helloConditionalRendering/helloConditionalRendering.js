623746bd26ae850f9e9ff65dbbf7529a49903cee
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2021-10-16 15:06:30 -0500

    Conditional Rendering match input
    conditional rendering based on input text

--- force-app/main/default/lwc/helloConditionalRendering/helloConditionalRendering.js
+++ force-app/main/default/lwc/helloConditionalRendering/helloConditionalRendering.js
@@ -2,8 +2,13 @@ import { LightningElement } from 'lwc';
 
 export default class HelloConditionalRendering extends LightningElement {
     isVisible = false
+    name
     handleClick(){
         this.isVisible=true
+    }
+
+    changeHandler(event){
+        this.name = event.target.value
     }
 
 }
